b1ea5e308b04aef90aec3ec1a63a565e8fac8b98
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-09 14:00:11 +0530

    Added level01 folder with files

---
+++ level01/Problem1.java.java
@@ -0,0 +1,15 @@
+class Problem1{
+	public static void main(String[] args)
+	{
+		//Birth year of Harry
+		int birthYear = 2000;
+		//Current year
+		int currentYear = 2024;
+		
+		//Calculating Harry's Age
+		int age = currentYear  - birthYear;
+
+		//Printing Harry's Age
+		System.out.println("Age of Harry is :-" +  age);
+	}
+}
