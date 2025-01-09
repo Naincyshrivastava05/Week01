4e0d8c189be43865d51bc562d078ae0b79c2fbcb
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-09 14:30:19 +0530

    Third Problem

---
+++ level01/Problem3.java
@@ -0,0 +1,14 @@
+class Problem3{
+
+	public static void main(String[] args){
+		//Value of Kilometer
+		float Kilometer= 10.8f;
+		//Value of 1 miles
+		float miles = 1.6f;
+
+		// Converting distance from Kilometerto Miles
+		float distance =  keloMeter *miles;
+		//Printing distance in miles
+		System.out.println("Distance in miles is:-" + distance);
+	}
+}
