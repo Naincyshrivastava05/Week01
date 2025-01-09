c50e1775523010175219efc0e70066d5e9de9ec4
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-01-09 14:27:06 +0530

    Adding Problem2

---
+++ level01/Problem2.java
@@ -0,0 +1,16 @@
+class Problem2{
+
+	public  static void main(String[] args){
+		//Marks of Maths
+		float mathMarks = 94;
+		//Marks of Physics
+		float physicsMarks = 95;
+		//Marks of Chemistry
+		float chemistryMarks = 96;
+	
+		//Calculating average percent mark in PCM	
+		float  avgPercent = (mathMarks + physicsMarks + chemistryMarks)/3;
+		//Printing average percent mark in PCM	
+		System.out.println("Average Percentage mark in PCM is:-" + avgPercent);
+	}
+}
